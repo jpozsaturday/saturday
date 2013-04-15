0179c955d46e02e7aa608615deec05a14ff76b5d
James Taco Pozdena <jpoz@simple.com>
2013-04-14 17:15:15 -0700

    makin is pretty

--- app.rb
+++ app.rb
@@ -2,11 +2,28 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  erb :index   
+  erb :index
+end
+
+get '/login' do
+  @username = params[:username]
+  @password = params[:password]
+  if @username == 'jpoz'
+    if @password == 'taco'
+      redirect "/name?name=jpoz&state=Oregon"
+    else
+      @error = "Wrong Password"
+      erb :index
+    end
+  else
+    @error = "Wrong username"
+    erb :index
+  end
 end
 
 get '/name' do
   @name = (params[:name] || "Nobody").downcase
+  @state = params[:state]
   if @name == "collin"
     redirect 'http://www.youtube.com/watch?v=oHg5SJYRHA0'
   else
