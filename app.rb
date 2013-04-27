fdeade77eb4fbd033df82d1199cd54cfae89b23c
James Taco Pozdena <jpoz@simple.com>
2013-04-27 09:40:30 -0700

    Session 4

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
 get '/' do
   erb :index
@@ -13,6 +14,22 @@ get '/login' do
       redirect "/name?name=jpoz&state=Oregon"
     else
       @error = "Wrong Password"
+      erb :index
+    end
+  else
+    @error = "Wrong username"
+    erb :index
+  end
+end
+
+post '/login' do
+  @username = params[:username]
+  @password = params[:password]
+  if @username == 'jpoz'
+    if @password == 'banana'
+      erb :home
+    else
+      @error = "Hey dude you got the password wrong"
       erb :index
     end
   else
